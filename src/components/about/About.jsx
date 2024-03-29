96eb2bcd8a4b62643538ff6d356e6836fd6f9cbe
eruditeme <evangelineyiqin@gmail.com>
2024-03-29 03:06:30 -0400

    added cherry blossom model

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,36 +3,22 @@ import './about.css'
 import { Canvas } from '@react-three/fiber'
 import { useGLTF, OrbitControls } from '@react-three/drei'
 
-function Model(props) {
-	const { nodes, materials } = useGLTF('/ice_cream_soda.glb')
+export function Cherry(props) {
+	const { nodes, materials } = useGLTF('/cherry_blossom.glb')
 	return (
 	  <group {...props} dispose={null}>
 		<group scale={0.01}>
-		  <mesh geometry={nodes.Glass_Glass_0.geometry} material={materials.Glass} rotation={[-Math.PI / 2, 0, 0]} scale={100} />
-		  <mesh geometry={nodes.Soda_Soda_0.geometry} material={materials.Soda} rotation={[-Math.PI / 2, 0, 0]} scale={100} />
-		  <mesh geometry={nodes.IceCream_IceCreamVanilla_0.geometry} material={materials['IceCream.Vanilla']} position={[-2.344, 322.816, 1.269]} rotation={[-1.468, 0.084, 2.454]} scale={53.393} />
-		  <mesh geometry={nodes.Cherry_Cherry_0.geometry} material={materials.Cherry} position={[44.337, 325.215, -32.749]} rotation={[-2.15, -0.268, -0.533]} scale={84.972} />
-		  <mesh geometry={nodes.Straw_Straw_0.geometry} material={materials.Straw} position={[-9.462, 68.945, 5.619]} rotation={[-1.509, 0.24, -1.886]} scale={100} />
-		  <mesh geometry={nodes.Bubbles_Bubble_0.geometry} material={materials.Bubble} rotation={[-Math.PI / 2, 0, 0]} scale={100} />
-		  <mesh geometry={nodes.Ice001_Ice_0.geometry} material={materials.material} position={[-24.888, 289.766, -18.712]} rotation={[-1.625, 0.51, 0.11]} scale={91.637} />
-		  <mesh geometry={nodes.Ice002_Ice_0.geometry} material={materials.material} position={[36.432, 289.766, -1.634]} rotation={[-1.28, -0.125, -0.519]} scale={71.156} />
-		  <mesh geometry={nodes.Ice003_Ice_0.geometry} material={materials.material} position={[17.595, 294.356, -29.479]} rotation={[-1.776, -0.356, -2.295]} scale={72.305} />
-		  <mesh geometry={nodes.Ice004_Ice_0.geometry} material={materials.material} position={[8.277, 294.356, 16.478]} rotation={[-1.088, -0.184, -1.065]} scale={77.093} />
-		  <mesh geometry={nodes.Ice005_Ice_0.geometry} material={materials.material} position={[-25.157, 289.766, 25.192]} rotation={[-1.803, 0.216, 2.279]} scale={82.853} />
-		  <mesh geometry={nodes.Ice006_Ice_0.geometry} material={materials.material} position={[-36.949, 252.127, 16.478]} rotation={[-1.088, -0.184, -1.065]} scale={77.093} />
-		  <mesh geometry={nodes.Ice007_Ice_0.geometry} material={materials.material} position={[12.492, 248.297, 31.938]} rotation={[-1.423, -0.259, -2.616]} scale={91.001} />
-		  <mesh geometry={nodes.Ice008_Ice_0.geometry} material={materials.material} position={[-36.949, 253.304, -33.739]} rotation={[-1.088, -0.184, -1.065]} scale={77.093} />
-		  <mesh geometry={nodes.Ice009_Ice_0.geometry} material={materials.material} position={[7.95, 257.175, -9.154]} rotation={[-0.985, -0.601, -0.802]} scale={77.093} />
+		  <mesh geometry={nodes.cherry_blossom_cherry_blossom_0.geometry} material={materials.cherry_blossom} rotation={[-Math.PI / 2, 0, 0]} />
 		</group>
 	  </group>
 	)
-  }
+}
 
 const About = () => {
   return (
 	<section className='about'>
-		<div className="hazard-background mt-32 mb-20"></div>
-		<div className='lg:grid lg:grid-cols-5'>
+		<div className='lg:mt-32 lg:mb-8'><div id="scissors"></div></div>
+		<div className='lg:grid lg:grid-cols-6'>
 			<div className='col-span-3'>
 				<div className='lg:text-4xl ml-20 arimo-uniquifier font-extrabold'>
 					About me
@@ -61,12 +47,11 @@ const About = () => {
 					</div>
 				</div>
 			</div>
-			{/* "Akira Bike" (https://skfb.ly/o9nwM) by Ryanwill679 is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/). -*/}
-			<div className='h-screen col-span-2 flex justify-center items-end mt-5'>
-				<Canvas camera={{ fov: 40, position: [3, 3, 3] }}>
-					<ambientLight intensity={4.2} />
+			<div className='h-screen col-span-3 flex justify-center items-end ml-20'>
+				<Canvas camera={{ fov: 32, position: [-4, 3, 38] }}>
+				<ambientLight intensity={2.4} />
 					<OrbitControls className='autoRotate' enableZoom={false} />
-					<Model scale={0.49}/>
+					<Cherry scale={4}/>
 				</Canvas>
 			</div>
 		</div>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './header.css'
-import ama from "../../assets/ama.jpeg"
+import fi from "../../assets/fi.png"
 
 
 const Header = () => {
@@ -8,17 +8,18 @@ const Header = () => {
 	<section className='about'>
 		<div className='lg:grid lg:grid-cols-3'>
 			<hr className="col-span-2 horizontal-line mt-10"/>
-			<div className='absolute top-1/2 col-span-2 anton-regular xs:text-5xl xs:mt-2 xs:ml-4 sm:mt-3 sm:text-6xl md:text-7xl md:mt-4 md: ml-4 lg:mt-8 lg:ml-20 lg:text-8xl xl:text-9xl'>
+			<div className='z-10 absolute top-1/2 col-span-2 anton-regular xs:text-5xl xs:mt-2 xs:ml-4 sm:mt-3 sm:text-6xl md:text-7xl md:mt-4 md: ml-4 lg:mt-8 lg:ml-20 lg:text-8xl xl:text-9xl'>
 				portfolio ver. 02
 				<div className='xl:text-3xl lg:text-2xl md:text-xl sm:text-lg xs:text-sm lg:mt-3 right-2 absolute font-thin tracking-wider'>
 					Evangeline Lam
 				</div>
 			</div>
-			<div className='col-span-1 justify-center flex items-center'>
-				<div className='z-10 absolute right-20 top-2/3 text-8xl flex flex-col font-extrabold anton-regular'>
-					<span>蓝</span>
-					<span>伊</span>
-					<span>心</span>
+			<img className='absolute right-8 z-0 bottom-0 xs:h-2/5 sm:h-1/2 md:h-3/4 lg:h-4/5' src={fi} alt='fibonacci sequence'/>
+			<div className='col-span-1 justify-center flex'>
+				<div className='z-10 absolute xs:right-8 lg:right-20 xs:top-3/4 lg:top-2/3 xs:text-5xl lg:text-8xl flex flex-col anton-regular'>
+					<span className='font-semibold'>蓝</span>
+					<span className='font-semibold'>伊</span>
+					<span className='font-semibold'>沁</span> 
 				</div>
 			</div>
 		</div>
